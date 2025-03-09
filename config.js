883208400a39f562fab1bedd8518bc26fbf0486c
alfah101 <101655761+alfah101@users.noreply.github.com>
2025-03-09 15:54:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -67,7 +67,7 @@ hbday: {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "sofiya.mp3", // Music streaming URL
+        musicUrl: "https://github.com/alfah101/happy-bday-sofiya/blob/main/Sofiya.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
